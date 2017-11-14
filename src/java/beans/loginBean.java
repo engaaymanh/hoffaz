16ab8a25ea630703d577516f37370f605bb2d316
Khaled-Tayem <ktayem81@gmail.com>
2017-11-14 09:01:10 +0200

    draft of user interface.

---
+++ src/java/beans/loginBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author khale
+ */
+@Named(value = "loginBean")
+@Dependent
+public class loginBean {
+
+    /**
+     * Creates a new instance of loginBean
+     */
+    public loginBean() {
+    }
+    
+}
